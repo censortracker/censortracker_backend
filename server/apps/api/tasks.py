4402367a78b78110a3051f07db8ebccb7b259a7c
bystrukhin <bystruhin@gmail.com>
2020-07-29 15:16:17 +0700

    Add ip data updating task

---
+++ server/apps/api/tasks.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+
+"""
+Celery Task for news grabber.
+"""
+
+from celery.schedules import crontab
+from celery.task import PeriodicTask
+from django.core.management import call_command
+
+
+class IPData(PeriodicTask):
+    name = 'Update IP data'
+    run_every = crontab(hour=3)
+
+    def run(self, *args, **kwargs):
+        call_command('update_ip_data')
+        return True
