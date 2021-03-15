a871c11f4150ae255c4faad58d8489318fe38ee5
Isaak Uchakaev <likid.geimfari@gmail.com>
2021-03-15 15:55:22 +0300

    Fix admin

--- server/settings/components/common.py
+++ server/settings/components/common.py
@@ -48,7 +48,9 @@ MIDDLEWARE = (
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.locale.LocaleMiddleware",
     "django.middleware.common.CommonMiddleware",
+    "django.middleware.csrf.CsrfViewMiddleware",
     "django.contrib.auth.middleware.AuthenticationMiddleware",
+    "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
 )
 
