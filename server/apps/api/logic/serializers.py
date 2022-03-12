d85eb5fb285bac805aad723c5739e948af91b006
Isaak Uchakaev <likid.geimfari@gmail.com>
2022-03-12 14:55:57 +0300

    Revert changes related to serializers

--- server/apps/api/logic/serializers.py
+++ server/apps/api/logic/serializers.py
@@ -36,4 +36,4 @@ class ConfigSerializer(serializers.ModelSerializer):
 class ProxyConfigSerializer(serializers.ModelSerializer):
     class Meta:
         model = ProxyConfig
-        exclude = ("id", "name", "ping_host", "ping_port", "priority")
+        exclude = ("id", "name", "priority")
