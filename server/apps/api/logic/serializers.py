ee26c0f6cd4f3d617e0cf1afbeafd9a5fbd72b05
Isaak Uchakaev <likid.geimfari@gmail.com>
2022-03-19 21:56:09 +0300

    Fix api endpoint

--- server/apps/api/logic/serializers.py
+++ server/apps/api/logic/serializers.py
@@ -36,4 +36,4 @@ class ConfigSerializer(serializers.ModelSerializer):
 class ProxyConfigSerializer(serializers.ModelSerializer):
     class Meta:
         model = ProxyConfig
-        exclude = ("id", "name", "priority", "active")
+        exclude = ("id", "name", "weight", "active")
