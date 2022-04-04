25a0502a2fa0213a6577cbe8cf87a39582fafddb
Isaak Uchakaev <likid.geimfari@gmail.com>
2022-04-04 09:28:33 +0300

    Fix urls

--- server/apps/api/urls.py
+++ server/apps/api/urls.py
@@ -10,12 +10,12 @@ urlpatterns = [
     path("countries/", views.CountryListView.as_view(), name="country_list"),
     path("config/", views.ConfigRetrieveAPIView.as_view(), name="config_detail"),
     path(
-        "proxy/update/<str:name>/",
+        "proxy/update/<str:name>",
         views.ProxyConfigUpdateAPIView.as_view(),
         name="proxy_update",
     ),
     path(
-        "proxy/create/",
+        "proxy/create",
         views.ProxyConfigCreateAPIView.as_view(),
         name="proxy_create",
     ),
