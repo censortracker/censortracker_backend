5f2b6b008cdaff0371be828eeb97094656a80f67
Isaak Uchakaev <likid.geimfari@gmail.com>
2022-11-21 11:49:38 +0300

    Fix rate limit

--- server/apps/api/logic/throttling.py
+++ server/apps/api/logic/throttling.py
@@ -3,18 +3,18 @@ from rest_framework.throttling import AnonRateThrottle
 
 
 class CountryListRateThrottle(AnonRateThrottle):
-    rate = "5000/day"
+    rate = "300000/day"
     caches = caches["api"]
     scope = "country_list_anon"
 
 
 class ProxyConfigListRateThrottle(AnonRateThrottle):
-    rate = "10000/day"
+    rate = "300000/day"
     caches = caches["api"]
     scope = "proxy_config_list_anon"
 
 
 class ConfigRetrieveRateThrottle(AnonRateThrottle):
-    rate = "10000/day"
+    rate = "300000/day"
     caches = caches["api"]
     scope = "retrieve_config_anon"
