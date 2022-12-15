9ed23aa29bd6e1289a0c8fa35f58b00b66dcf23f
Isaak Uchakaev <likid.geimfari@gmail.com>
2022-12-15 21:14:43 +0300

    Fix formatting

--- server/settings/components/common.py
+++ server/settings/components/common.py
@@ -50,7 +50,7 @@ MIDDLEWARE = (
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.locale.LocaleMiddleware",
-    'corsheaders.middleware.CorsMiddleware',
+    "corsheaders.middleware.CorsMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
     "django.contrib.auth.middleware.AuthenticationMiddleware",
