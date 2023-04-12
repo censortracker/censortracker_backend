75c22c3020cd2b09c94217e692db731cb904905c
mva <mva@roskomsvoboda.org>
2023-04-12 19:15:41 +0700

    env: support to load files from code root on local deploy

--- server/settings/components/__init__.py
+++ server/settings/components/__init__.py
@@ -14,6 +14,25 @@ def err(msg, fatal=True):
         raise Exception(msg)
 
 
+def is_file(file_path):
+    return Path(file_path).is_file()
+
+
+localdev_env_path = BASE_DIR
+server_env_path = BASE_DIR.parent.parent
+if is_file("%s/.env" % (server_env_path)):
+    env_path = server_env_path
+elif is_file("%s/.env" % (localdev_env_path)):
+    env_path = localdev_env_path
+else:
+    err(
+        "Can't distinguish whether we deploying on dev's machine or server. " +
+        "Please, create .env file either in %s or %s" %
+        (env_path, env_path.parent.parent),
+        True
+    )
+
+
 def keepcast(what=None):
     """
     Callable "caster" to keep current var. type
@@ -24,7 +43,7 @@ def keepcast(what=None):
 
 def strtobool(value):
     TRUE_VALUES = {"y", "yes", "t", "true", "on", "1"}
-    FALSE_VALUES = {"n", "no", "f", "false", "off", "0",""}
+    FALSE_VALUES = {"n", "no", "f", "false", "off", "0", ""}
     if isinstance(value, bool):
         return value
     value = value.lower()
@@ -45,9 +64,9 @@ def _load_file(file_name, fatal=True):
     """
     Loader for .env files
     """
-    file_path = PurePath("%s/%s" % (BASE_DIR.parent.parent, file_name))
-    if os.path.isfile(file_path):
-        data = {}
+    file_path = PurePath("%s/%s" % (env_path, file_name))
+    data = {}
+    if is_file(file_path):
         with open(file_path, "r", encoding='UTF-8') as envfile:
             for line in envfile:
                 line = line.strip()
@@ -56,11 +75,13 @@ def _load_file(file_name, fatal=True):
                 k, v = line.split('=', 1)
                 k = k.strip()
                 v = v.strip()
-                if len(v) >= 2 and ((v[0] == "'" and v[-1] == "'") or (v[0] == '"' and v[-1] == '"')):
+                if len(v) >= 2 and ((v[0] == "'" and v[-1] == "'")
+                                    or (v[0] == '"' and v[-1] == '"')):
                     v = v[1:-1]
                 data[k] = v
     else:
-        err('File "%s" ("%s") is missing' % (file_name, file_path), fatal)
+        err("No such file: %s" % (file_path), (file_name == ".env"))
+
     return data
 
 
@@ -78,7 +99,8 @@ def env(var, fatal=True, default=None, cast=keepcast):
     if not val:
         if default is not None:
             fatal = False
-        err('"%s" is neither defined in system environment, neither can be found in .env/.env.ci files' % (var), fatal)
+        err('"%s" is neither defined in system environment, ' +
+            'neither can be found in .env/.env.ci files' % (var), fatal)
         val = default
     if cast is bool:
         cast = boolcast
