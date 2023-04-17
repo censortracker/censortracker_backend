b1f28748ec05966aba2af5bee035338fd65d4e33
Isaak Uchakaev <likid.geimfari@gmail.com>
2023-04-17 16:39:13 +0300

    Add user to admin

--- server/apps/users/admin.py
+++ server/apps/users/admin.py
@@ -1,1 +1,10 @@
 from django.contrib import admin
+
+from server.apps.users.models import User
+
+
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ("email", "is_staff", "is_active", "date_joined")
+    search_fields = ("email",)
+    ordering = ("email",)
